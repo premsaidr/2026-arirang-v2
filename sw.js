2ded4f8c61cb31c652814ee1d20fd277dbd4710d
premsaidr <premsai.dr@gmail.com>
2026-05-19 00:19:33 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
  *  - 인증된 응답만 캐싱하여 오프라인 시 안전하게 제공
  * ============================================================ */
 
-const CACHE_NAME = 'jsd2026-v1.2.4';
+const CACHE_NAME = 'jsd2026-v1.3.0';
 
 const APP_SHELL = [
   '/',
@@ -22,7 +22,10 @@ const APP_SHELL = [
   '/favicon-32.png',
   '/favicon-48.png',
   '/favicon-64.png',
+  '/favicon-72.png',
+  '/favicon-96.png',
   '/favicon-128.png',
+  '/favicon-144.png',
   '/favicon-180.png',
   '/favicon-192.png',
   '/favicon-512.png',
